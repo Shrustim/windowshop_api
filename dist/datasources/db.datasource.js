1fbb84499acec9a7ddfe589b8731f6f04817e3b0
shrushti mane <shrustimane1998@gmail.com>
2021-12-12 12:20:42 +0530

    db change

--- dist/datasources/db.datasource.js
+++ dist/datasources/db.datasource.js
@@ -5,14 +5,22 @@ const tslib_1 = require("tslib");
 const core_1 = require("@loopback/core");
 const repository_1 = require("@loopback/repository");
 const config = {
+    // name: 'db',
+    // connector: 'mysql',
+    // url: '',
+    // host: 'sql5.freesqldatabase.com',
+    // port: 3306,
+    // user: 'sql5449233',
+    // password: 'vY5PnXUB7X',
+    // database: 'sql5449233'
     name: 'db',
     connector: 'mysql',
     url: '',
-    host: 'sql5.freesqldatabase.com',
+    host: 'www.db4free.net',
     port: 3306,
-    user: 'sql5449233',
-    password: 'vY5PnXUB7X',
-    database: 'sql5449233'
+    user: 'clgpro_decore',
+    password: 'clgProDecore@Akki',
+    database: 'clgprodecoredb'
 };
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -2,14 +2,23 @@ import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
 const config = {
+  // name: 'db',
+  // connector: 'mysql',
+  // url: '',
+  // host: 'sql5.freesqldatabase.com',
+  // port: 3306,
+  // user: 'sql5449233',
+  // password: 'vY5PnXUB7X',
+  // database: 'sql5449233'
+
   name: 'db',
   connector: 'mysql',
   url: '',
-  host: 'sql5.freesqldatabase.com',
+  host: 'www.db4free.net',
   port: 3306,
-  user: 'sql5449233',
-  password: 'vY5PnXUB7X',
-  database: 'sql5449233'
+  user: 'clgpro_decore',
+  password: 'clgProDecore@Akki',
+  database: 'clgprodecoredb'
 };
 
 // Observe application's life cycle to disconnect the datasource when
